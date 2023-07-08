dd98c9df0f1469fe766b4e05bfb8b95e87e4d487
Nishant Adhav <121330191+NishantAdhav@users.noreply.github.com>
2023-07-08 23:03:35 +0530

    Add files via upload

---
+++ triplet_with_zero_sum.cpp
@@ -0,0 +1,81 @@
+/*Given an array arr[] of n integers. Check whether it contains a triplet that sums up to zero. 
+
+Note: Return 1, if there is at least one triplet following the condition else return 0.
+
+Example 1:
+
+Input: n = 5, arr[] = {0, -1, 2, -3, 1}
+Output: 1
+Explanation: 0, -1 and 1 forms a triplet
+with sum equal to 0.
+Example 2:
+
+Input: n = 3, arr[] = {1, 2, 3}
+Output: 0
+Explanation: No triplet with zero sum exists. 
+
+Your Task:
+You don't need to read input or print anything. Your task is to complete the boolean function findTriplets() which takes the array arr[] and the size of the array (n) as inputs and print 1 if the function returns true else print 0 if the function returns false. 
+
+Expected Time Complexity: O(n2)
+Expected Auxiliary Space: O(1)
+*/
+
+
+#include<iostream>
+#include<stdlib.h>
+#include<iostream>
+using namespace std;
+
+// } Driver Code Ends
+/* You are required to complete the function below
+*  arr[]: input array
+*  n: size of array
+*/
+class Solution{
+  public:
+    //Function to find triplets with zero sum.
+    bool findTriplets(int arr[], int n)
+    { 
+      sort(arr, arr + n);
+    for (int i = 0; i < n - 2; i++) {
+        int base = arr[i];
+        int left = i + 1;
+        int right = n - 1; // Corrected initialization to n - 1
+
+        while (left < right) {
+            int current_sum = base + arr[left] + arr[right];
+
+            if (current_sum == 0) {
+                return true;
+            } else if (current_sum < 0) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+    }
+    return false;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+	cin>>t;
+	while(t--){
+    	int n;
+    	cin>>n;
+    	int arr[n]={0};
+    	for(int i=0;i<n;i++)
+    		cin>>arr[i];
+    	Solution obj;
+        if(obj.findTriplets(arr, n))
+            cout<<"1"<<endl;
+        else 
+            cout<<"0"<<endl;
+	}
+    return 0;
+}
+// } Driver Code Ends
