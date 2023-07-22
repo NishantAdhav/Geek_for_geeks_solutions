74c599f9d7307fb1ac640d1a34179c571f5b5552
Nishant Adhav <121330191+NishantAdhav@users.noreply.github.com>
2023-07-22 20:11:23 +0530

    Add files via upload

---
+++ reverse_linked_list.cpp
@@ -0,0 +1,152 @@
+/*Given a linked list of size N. The task is to reverse every k nodes (where k is an input to the function) in the linked list. If the number of nodes is not a multiple of k then left-out nodes, in the end, should be considered as a group and must be reversed (See Example 2 for clarification).
+
+Example 1:
+
+Input:
+LinkedList: 1->2->2->4->5->6->7->8
+K = 4
+Output: 4 2 2 1 8 7 6 5 
+Explanation: 
+The first 4 elements 1,2,2,4 are reversed first 
+and then the next 4 elements 5,6,7,8. Hence, the 
+resultant linked list is 4->2->2->1->8->7->6->5.
+Example 2:
+
+Input:
+LinkedList: 1->2->3->4->5
+K = 3
+Output: 3 2 1 5 4 
+Explanation: 
+The first 3 elements are 1,2,3 are reversed 
+first and then elements 4,5 are reversed.Hence, 
+the resultant linked list is 3->2->1->5->4.
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function reverse() which should reverse the linked list in group of size k and return the head of the modified linked list.
+
+Expected Time Complexity : O(N)
+Expected Auxilliary Space : O(1)
+
+Constraints:
+1 <= N <= 105
+1 <= k <= N
+*/
+//{ Driver Code Starts
+#include<iostream>
+using namespace std;
+
+
+struct node
+{
+    int data;
+    struct node* next;
+    
+    node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+
+/* Function to print linked list */
+void printList(struct node *node)
+{
+    while (node != NULL)
+    {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    printf("\n");
+}
+
+
+// } Driver Code Ends
+
+  /*Reverse a linked list
+  The input list will have at least one element  
+  Return the node which points to the head of the new LinkedList
+  Node is defined as 
+  */
+        
+ 
+
+
+class Solution
+{
+    public:
+    struct node *reverse (struct node *head, int k)
+    { 
+        node*curr_head,*next_head;
+        curr_head=next_head=nullptr;
+        
+        function<void(node*,node*,int)>traverse=[&](node*cur,node*prev,int p)
+        {
+            if(cur==nullptr)
+            return;
+            
+            traverse(cur->next,cur,(p+1)%k);
+            
+            if((cur->next==nullptr) or (p==k-1))
+            curr_head=cur;
+            
+            if(p==0)
+            {
+                cur->next=next_head;
+                next_head=curr_head;
+            }
+            else
+            {
+                cur->next=prev;
+            }
+        };
+        traverse(head,nullptr,0);
+        return curr_head;
+
+
+    }
+};
+
+
+//{ Driver Code Starts.
+
+/* Drier program to test above function*/
+int main(void)
+{
+    int t;
+    cin>>t;
+     
+    while(t--)
+    {
+        struct node* head = NULL;
+        struct node* temp = NULL;
+        int n;
+        cin >> n;
+         
+        for(int i=0 ; i<n ; i++)
+        {
+            int value;
+            cin >> value;
+            if(i == 0)
+            {
+                head = new node(value);
+                temp = head;
+            }
+            else
+            {
+                temp->next = new node(value);
+                temp = temp->next;
+            }
+        }
+        
+        int k;
+        cin>>k;
+        
+        Solution ob;
+        head = ob.reverse(head, k);
+        printList(head);
+    }
+     
+    return(0);
+}
+
+
+// } Driver Code Ends
