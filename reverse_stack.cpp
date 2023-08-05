509e0a62f41240443b1789df291055e7851ecee6
Nishant Adhav <121330191+NishantAdhav@users.noreply.github.com>
2023-08-05 22:57:32 +0530

    Add files via upload

---
+++ reverse_stack.cpp
@@ -0,0 +1,106 @@
+/*
+You are given a stack St. You have to reverse the stack using recursion.
+
+Example 1:
+
+Input:
+St = {3,2,1,7,6}
+Output:
+{6,7,1,2,3}
+Explanation:
+Input stack after reversing will look like the stack in the output.
+Example 2:
+
+Input:
+St = {4,3,9,6}
+Output:
+{6,9,3,4}
+Explanation:
+Input stack after reversing will look like the stack in the output.
+Your Task:
+
+You don't need to read input or print anything. Your task is to complete the function Reverse() which takes the stack St as input and reverses the given stack.
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(N)
+
+Constraints:
+1 <= size of the stack <= 104
+-109 <= Each element of the stack <= 109
+Sum of N over all test cases doesn't exceeds 106
+Array may contain duplicate elements. 
+*/
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <iostream>
+#include<vector>
+#include<stack>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+
+class Solution {
+public:
+    void insertAtBottom(stack<int> &St, int temp) {
+        if (St.empty()) 
+        {
+            St.push(temp);
+        } 
+        else 
+        {
+            int popped = St.top();
+            St.pop();
+            insertAtBottom(St, temp);
+            St.push(popped);
+        }
+    }
+
+    void Reverse(stack<int> &St){
+        if (St.size() <= 1) {
+            return;
+        }
+        int temp = St.top();
+        St.pop();
+        Reverse(St);
+        insertAtBottom(St, temp);
+    }
+};
+
+
+//{ Driver Code Starts.
+
+
+int main(){
+    int T;
+    cin>>T;
+    while(T--){
+        int N;
+        cin>>N;
+        stack<int> St;
+        for(int i=0;i<N;i++){
+            int x;
+            cin>>x;
+            St.push(x);
+        }
+        Solution ob;
+        ob.Reverse(St);
+        vector<int>res;
+        while(St.size())
+        {
+            res.push_back(St.top());
+            St.pop();
+        }
+        for(int i = res.size()-1;i>=0;i--)
+        {
+            cout<<res[i]<<" ";
+        }
+        
+        cout<<endl;
+    }
+}
+// } Driver Code Ends
+
